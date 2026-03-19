d8dde730d2f83ae83e98fbd3595594b074511ace
Cristian D Biancotti <cdbiancotti@gmail.com>
2026-03-18 22:20:12 -0300

    modifique el repaso2

--- repaso2.py
+++ repaso2.py
@@ -7,21 +7,3 @@
     
 # pepe(2, numero3=2, numero2=3)
 # pepe(2, numero3=2)
-
-#########################################################
-
-# print('pepe)
-
-# try:
-#     1/1
-# except ZeroDivisionError:
-#     print('Hubo un provblema de division por cero')
-# except TypeError:
-#     print('Hubo un error de tipos de datos')
-# except Exception:
-#     print('Hubo un error no protegido')
-# else:
-#     print('No hubo errores')
-
-
-# https://docs.python.org/es/3/library/exceptions.html#exception-hierarchy
