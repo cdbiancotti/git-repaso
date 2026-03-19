3d9bf3414da89fd239232b24b91b3e9518beefb7
Cristian D Biancotti <cdbiancotti@gmail.com>
2026-03-18 21:22:38 -0300

    avance en la rama prueba

--- repaso.py
+++ repaso.py
@@ -5,10 +5,10 @@
 # suma_que_muestra(2, 2)
 
 
-def suma_que_devuelve(num1, num2=0):
-    return num1 + num2
+# def suma_que_devuelve(num1, num2=0):
+#     return num1 + num2
     
 
-resultado_de_la_suma = suma_que_devuelve(2)
+# resultado_de_la_suma = suma_que_devuelve(2)
 
-print('El resutado de la suma es', resultado_de_la_suma)
+# print('El resutado de la suma es', resultado_de_la_suma)
